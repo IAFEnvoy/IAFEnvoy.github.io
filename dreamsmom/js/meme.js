490903c0c5aef3bc9e6085c2257566fda1a2bafe
IAFEnvoy <tanksheng545@126.com>
2022-03-23 16:46:45 +0800

    1

---
+++ dreamsmom/js/meme.js
@@ -0,0 +1,22 @@
+window.onload = function() {
+    var searchStr = location.search;
+    searchStr = searchStr.substr(1);
+    var searchs = searchStr.split("&");
+    var ids = searchs[0].split("=");
+    var this_url = document.baseURI.replace("meme.html", "").replace(location.search, "");
+    var url = this_url + "DataBase/meme/" + ids[1] + ".txt";
+    var request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.send(null);
+    request.onloadend = function() {
+        if (request.status == 200) {
+            const lexer = new marked.Lexer();
+            const tokens = lexer.lex(request.responseText);
+            const parser = new marked.Parser();
+            const html = parser.parse(tokens);
+            document.getElementById("data").innerHTML = html;
+        } else {
+            alert("这里还没有写完哦~");
+        }
+    }
+}
